16ab0b1ae893952871d0881d30f89861d6a0d3f8
Tomasz Lis <tomasz.lis@o2.pl>
2018-09-17 20:25:12 +0200

    Update excercise

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 'use scrict';
 
+// Datas links veriables
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = document.getElementById('countries');
+
+// Pin on "Search button"
+document.getElementById('search').addEventListener('click', searchCountries);
+
+// Function for search countries
+function searchCountries() {
+	var countryName = document.getElementById('country-name').value;
+	if (!countryName.length) countryName = 'Poland';
+	fetch(url + countryName, { cache: 'no-store' })
+		.then(function(resp) {
+			return resp.json();
+		})
+		.then(showCountriesList);
+}
